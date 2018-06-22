a48077e8e00a8932b2460cd4a9136e089a029581
Rodrigo Caldeira <rod.cdr@gmail.com>
2018-06-22 16:39:47 -0300

    Adicionado a função de percorrer os intervalos de m (de 300 em 300)
    Adicionado também função para fazer a otimização do m. Talvez seja
    preciso fazer ajuste fino - linha 163.

--- trab2/src/br/inf/ufes/attack/AttackClient.java
+++ trab2/src/br/inf/ufes/attack/AttackClient.java
@@ -28,12 +28,15 @@ public class AttackClient {
 		String dicPath = args[1]; //path to dctionary
 		String inFilePath = args[2]; //path to encrypted file
 		String knownWord = args[3]; //know word in the file
+		Boolean optimize = false;
 		ArrayList<String> _dict;
 		String cryptKey = null;
 		int size = 0;
 		if(args.length == 5) {
 			size = Integer.parseInt(args[4]); //size in bytes of the encrypted file
-		} 
+		} else if (args.length == 6) {
+			optimize = (args[5] == "true")?true:false;
+		}
 			
 		File inFile = new File(inFilePath);
 		byte[] bytes;
@@ -59,8 +62,8 @@ public class AttackClient {
 		if(!inFile.exists()) {
 			if(size == 0) {
 				Random r = new Random();
-				//random size between 1000 and 100000
-				size = r.nextInt(99000) + 1000 - knownWord.length();
+				//random size between 50000 and 200000
+				size = r.nextInt(150000) + 50000 - knownWord.length();
 			}
 			
 			bytes = new byte[size];
@@ -94,14 +97,77 @@ public class AttackClient {
 		try {
 			registry = LocateRegistry.getRegistry(host);
 			Attacker mestre = (Attacker) registry.lookup("mestre");
-			long startTime = System.nanoTime();
-			int attackSize = 4000; //TODO: ajustar isso para colocar num loop quando for testar
-			Guess[] guesses = mestre.attack(criptedFile, knownWord.getBytes(), attackSize);
-			double time_diff = (System.nanoTime() - startTime)/1000000;
-			System.out.println(time_diff);
-			for(Guess g : guesses) {
-				saveFile(g.getKey() + ".msg", g.getMessage());
+			long startTime;// = System.nanoTime();
+			Guess[] guesses;
+
+			int newAttackTimeSize = -1;
+			if (!optimize) {
+				for (int i = 100; i <= 40000; i+=300) {
+					startTime = System.nanoTime();
+					guesses = mestre.attack(criptedFile, knownWord.getBytes(), i);
+					double time_diff = (System.nanoTime() - startTime)/1000000;
+					System.out.println(i + ";" + time_diff + ";" + 	size);
+					for(Guess g : guesses) {
+						saveFile(g.getKey() + ".msg", g.getMessage());
+					}	
+				}
+			} else {
+				// first run to min attack size
+				int minAttackSize = 100;
+				int maxAttackSize = 40000;
+				startTime = System.nanoTime();
+				guesses = mestre.attack(criptedFile, knownWord.getBytes(), minAttackSize);
+				double shortAttackTime = (System.nanoTime() - startTime)/1000000;
+				startTime = System.nanoTime();
+				guesses = mestre.attack(criptedFile, knownWord.getBytes(), maxAttackSize);
+				double longAttackTime = (System.nanoTime() - startTime)/1000000;
+				double optimumTime = 9999999.0;
+				
+				double minAttackTime;
+				double maxAttackTime;
+				int minAttackTimeSize;
+				int maxAttackTimeSize;
+				
+				if (longAttackTime >= shortAttackTime) {
+					maxAttackTime = longAttackTime;
+					maxAttackTimeSize = maxAttackSize;
+					minAttackTime = shortAttackTime;
+					minAttackTimeSize = minAttackSize;
+				} else {
+					minAttackTime = longAttackTime;
+					minAttackTimeSize = maxAttackSize;
+					maxAttackTime = shortAttackTime;
+					maxAttackTimeSize = minAttackSize;
+				}
+				
+				//int actualAttackSize = (minAttackSize + maxAttackSize)/2;
+				double attackTime = minAttackTime;
+				int count = 0;
+				int repeat = 0;
+				while (count < 10000 || repeat < 3) {
+					System.out.println("Attack #" + (count+1));
+					newAttackTimeSize = ((minAttackTimeSize + maxAttackTimeSize)/2);
+					startTime = System.nanoTime();
+					guesses = mestre.attack(criptedFile, knownWord.getBytes(), newAttackTimeSize);
+					double newAttackTime = (System.nanoTime() - startTime)/1000000;
+					// novo atack esta no intervalo
+					if (newAttackTime >= minAttackTime && newAttackTime <= maxAttackTime) {
+						maxAttackTimeSize = newAttackTimeSize;
+						maxAttackTime = newAttackTime;
+					} else if (newAttackTime <= minAttackTime) {
+						maxAttackTimeSize = minAttackTimeSize;
+						maxAttackTime = minAttackTime;
+						minAttackTimeSize = newAttackTimeSize;
+						minAttackTime = newAttackTime;
+					}
+					
+					if ((newAttackTimeSize == ((minAttackTimeSize + maxAttackTimeSize)/2)) || (Math.abs(newAttackTime - attackTime) < 0.003)) repeat++;
+					count++;
+				}
+				System.out.println("count: " + count + " repeat: " + repeat + " bestAttackSize: " + newAttackTimeSize);
 			}
+			//int attackSize = 4000; //TODO: ajustar isso para colocar num loop quando for testar
+			
 		} catch (NotBoundException | IOException e) {
 			e.printStackTrace();
 		}
